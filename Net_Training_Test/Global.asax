0e5c7bc4c2448f2f692a19ffd22470ddf542f2a1
V_SayMAN <sayman0001111@gmail.com>
2016-06-17 19:45:14 +0300

    Добавьте файлы проекта.

---
+++ Net_Training_Test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Net_Training_Test.MvcApplication" Language="C#" %>
---
+++ Net_Training_Test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Net_Training_Test.Startup))]
+namespace Net_Training_Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
